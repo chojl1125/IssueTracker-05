9a5ad89701579cc38882dce5666ff20ed0487e8e
chojl1125 <chojl1125@gmail.com>
2020-10-29 12:19:35 +0900

    feat: Id & Password 유효성 검증 적용
    Validation Checker 를 통해 유효성 검증

--- iOS/IssueTracker/SignInViewController.swift
+++ iOS/IssueTracker/SignInViewController.swift
@@ -22,21 +22,50 @@ class SignInViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setUI()
+        bindUI()
     }
     
     func setUI() {
         
         idTextField.setLabel("아이디")
         passwordTextField.setLabel("비밀번호")
+        
     }
     
-
+    func bindUI() {
+        idTextField.rx.text.orEmpty.map(IdValidationChecker().validate).distinctUntilChanged()
+            .subscribe(onNext: { text in
+                
+                switch text {
+                case .invalidIdlength:
+                    self.idTextField.layer.borderColor = UIColor.red.cgColor
+                    self.idTextField.layer.borderWidth = 1.0
+                default:
+                    self.idTextField.layer.borderWidth = 0.0
+                }
+                
+        }).disposed(by: disposeBag)
+        
+        passwordTextField.rx.text.orEmpty.map(PasswordValidationChecker().validate).distinctUntilChanged().subscribe(onNext: { text in
+            
+            switch text {
+            case .invalidPwlength:
+                self.passwordTextField.layer.borderColor = UIColor.red.cgColor
+                self.passwordTextField.layer.borderWidth = 1.0
+            default:
+                self.passwordTextField.layer.borderWidth = 0.0
+            }
+            
+        })
+    }
+    
+    
 }
 
 extension UITextField {
     func setLabel(_ text: String) {
         
-        let label = UILabel(frame: CGRect(x: 10, y: 5, width: 10, height: 10))
+        let label = UILabel(frame: CGRect(x: 30, y: 5, width: 10, height: 10))
         label.text = text
         label.font = UIFont(name: label.font.fontName, size: 14)
         leftView = label
